1cc444d584cde1cb9fc4d1ed205112e64e00c5a3
Rodrigo <rodrigomxsantos@gmail.com>
2021-07-14 00:52:41 -0300

    Inicio do alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,41 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { AlurakutStyles } from "../src/lib/AluraCommons";
 
 const GlobalStyle = createGlobalStyle`
+
+* {
+  margim:0;
+  padding:0;
+  box-sizing: border-box;
+
+}
+
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #C0C0C0;
   }
-`
+
+  #next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: colum;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    
+  }
+
+  ${AlurakutStyles}
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +45,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,67 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid/injdex";
+import Box from "../src/components/Box/index";
+import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/AluraCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(propriedades) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${propriedades.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const usuario = `rodrigoxsantos`;
+  const pessoasFavoritas = [
+    `peas`,
+    `felipefialho`,
+    `rafaballerini`,
+    `juunegreiros`,
+    `omariosouto`,
+    `marcobrunodev`,
+  ];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        {/* <Box style="gridArea: profileArea;"> */}
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={usuario} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
